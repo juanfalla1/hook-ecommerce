46810ff5be420a345e71333f67fc3b53d9a0472d
Juan Carlos García <juangfalla@gmail.com>
2025-07-22 06:56:03 -0400

    Migrar formulario de contacto a API con NodeMailer

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { CartProvider } from "./context/CartContext";
+import { CartProvider } from "./context/CartContext"; // 👈 Importamos el provider
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CartProvider>
+    <CartProvider> {/* 👈 Aquí envolvemos la App */}
       <App />
     </CartProvider>
   </React.StrictMode>
 );
+
