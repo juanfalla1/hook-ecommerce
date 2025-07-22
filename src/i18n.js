428a300e69172c95ed27597d03f4dea53c326279
Juan Carlos García <juangfalla@gmail.com>
2025-07-22 16:07:59 -0400

    Migrar formulario de contacto a API con NodeMailer

--- src/i18n.js
+++ src/i18n.js
@@ -1,16 +1,18 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector"; // <-- esto permite detectar idioma o usar lo seleccionado por el usuario
+
 import en from "./locales/en.json";
 import es from "./locales/es.json";
 
 i18n
+  .use(LanguageDetector) // <-- necesario para permitir selección manual o automática
   .use(initReactI18next)
   .init({
     resources: {
       en: { translation: en },
       es: { translation: es },
     },
-    lng: "es", // idioma por defecto
     fallbackLng: "es",
     interpolation: {
       escapeValue: false,
